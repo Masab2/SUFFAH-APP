2c37a67297665d598f6650d36fdf207684574b29
Masab Haider <118460529+Masab2@users.noreply.github.com>
2023-12-23 04:33:22 -0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:suffa_app/firebase_options.dart';
 import 'package:suffa_app/res/routes/routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -11,14 +17,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'SUFFA-APP',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-          useMaterial3: true,
-        ),
-        
-        getPages: AppRoutes.appRoutes(),
-        );
+      debugShowCheckedModeBanner: false,
+      title: 'SUFFA-APP',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      getPages: AppRoutes.appRoutes(),
+    );
   }
 }
